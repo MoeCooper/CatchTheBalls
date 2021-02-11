5d53881b31f32a35616d06659cce59160ba46e0b
Maurice <42690947+MoeCooper@users.noreply.github.com>
2021-02-11 06:07:04 -0600

    Add files via upload

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private readonly float leftLimit = -35;
+    private readonly float bottomLimit = -10;
+
+    void DogDestroy() {
+        if (transform.position.x < leftLimit) {
+            Destroy(gameObject);
+        }
+    }
+    void BallDestroy() {
+        if (transform.position.y < bottomLimit) {
+            Destroy(gameObject);
+        }
+    }
+    void Update()
+    {
+        DogDestroy();
+        BallDestroy();
+    }
+}
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolDown = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {     
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && coolDown <= 0){           
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = 1f;
+        }
+        coolDown -= Time.deltaTime;
+    }
+}
